4e795ff721db553b32eee65d4e8f1fc460a57732
Hay Mar Kyaw <mandalaytvsindiamotorcycles@gmail.com>
2024-11-04 15:38:57 +0700

    install and setup Vite Tailwind in React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div class="text-indigo-500 font-bold text-2xl text-center ">Hello World</div>
+    </>
+  )
+}
+
+export default App
